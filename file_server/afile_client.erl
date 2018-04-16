09a0430bb51393b89a2e162d012381f9389bae8d
danielxu <xwd.forever@gmail.com>
2018-04-16 18:34:41 +0800

    File Server

---
+++ file_server/afile_client.erl
@@ -0,0 +1,18 @@
+-module(afile_client).
+-export([start/1, loop/1]).
+
+% list_dir
+% get_file
+
+start(Dir) ->
+   spawn(afile_server, loop, [Dir])
+
+loop(Dir) ->
+    receive
+        {Client, list_dir} ->
+            Client ! {self(), file:list_dir(Dir)};
+        {Client, {get_file, File}} ->
+            Full = filename:join([Dir, File]),
+            Client ! {self(), file:read(Full)}
+    end,
+    loop(Dir).
